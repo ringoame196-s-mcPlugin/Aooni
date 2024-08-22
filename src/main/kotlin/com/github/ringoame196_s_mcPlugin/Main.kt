c65c73105bb3c1c93b14e8dc252deb435d51949e
りんご飴196 <kazumac41@gmail.com>
2024-08-22 22:12:48 +0900

    [変更] ファイル生成 コマンド

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,15 +1,17 @@
 package com.github.ringoame196_s_mcPlugin
 
+import com.github.ringoame196_s_mcPlugin.events.AooniChestSettingEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
         val plugin = this
-        server.pluginManager.registerEvents(Events(), plugin)
-        saveDefaultConfig()
-        // val command = getCommand("aooni")
-        // command!!.setExecutor(Command())
-        // command.tabCompleter = TabCompleter()
+        server.pluginManager.registerEvents(AooniChestSettingEvent(plugin), plugin)
+        saveDefaultConfig() // config生成
+        saveResource("game.yml", false)
+        val command = getCommand("aooni")
+        command!!.setExecutor(Command())
+        command.tabCompleter = TabCompleter()
     }
 }
