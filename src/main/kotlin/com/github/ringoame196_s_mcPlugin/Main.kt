42199239dde098222abb9e4e645d3ce0123814f6
りんご飴196 <kazumac41@gmail.com>
2024-08-22 23:00:45 +0900

    [変更] Main わかりやすいように順番を変えた

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,17 +1,19 @@
 package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.events.AooniChestSettingEvent
+import com.github.ringoame196_s_mcPlugin.events.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
         val plugin = this
-        server.pluginManager.registerEvents(AooniChestSettingEvent(plugin), plugin)
         saveDefaultConfig() // config生成
         saveResource("game.yml", false)
         val command = getCommand("aooni")
         command!!.setExecutor(Command())
         command.tabCompleter = TabCompleter()
+        server.pluginManager.registerEvents(AooniChestSettingEvent(plugin), plugin)
+        server.pluginManager.registerEvents(PlayerInteractEvent(plugin), plugin)
     }
 }
