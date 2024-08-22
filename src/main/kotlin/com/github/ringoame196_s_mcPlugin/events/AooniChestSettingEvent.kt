8a46a3327dffa31b1084191c3aae502aa04c4544
りんご飴196 <kazumac41@gmail.com>
2024-08-22 22:13:06 +0900

    [追加] chestの登録 削除Event

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/AooniChestSettingEvent.kt
@@ -0,0 +1,70 @@
+package com.github.ringoame196_s_mcPlugin.events
+
+import com.github.ringoame196_s_mcPlugin.managers.YmlManager
+import org.bukkit.ChatColor
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.plugin.Plugin
+import java.io.File
+
+class AooniChestSettingEvent(plugin: Plugin) : Listener {
+    private val config = plugin.config
+    private val ymlMaterial = YmlManager()
+    private val aooniWorld = config.getString("aooni_world")
+    private val gameData = File(plugin.dataFolder, "game.yml")
+    private val key = "aooniChests"
+
+    @EventHandler
+    fun onBlockPlace(e: BlockPlaceEvent) {
+        val player = e.player
+        val block = e.block
+        val world = player.world.name
+
+        if (isExecutionAllowed(player, block)) {
+            // 設定されたワールドでOPがチェストを置いた場合のみ動かす
+            val message = "${ChatColor.GOLD}チェストを登録しました"
+            val chestLocation = block.location
+            additionChest(chestLocation)
+            player.sendMessage(message)
+        }
+    }
+
+    @EventHandler
+    fun onBlockBreak(e: BlockBreakEvent) {
+        val player = e.player
+        val block = e.block
+        if (isExecutionAllowed(player, block)) {
+            // 設定されたワールドでOPがチェストを破壊した場合のみ動かす
+            val message = "${ChatColor.RED}チェストを削除しました"
+            val chestLocation = block.location
+            deletionChest(chestLocation)
+            player.sendMessage(message)
+        }
+    }
+
+    private fun isExecutionAllowed(player: Player, block: Block): Boolean {
+        val world = player.world.name
+        val type = block.type
+        return world == aooniWorld && type == Material.CHEST && player.isOp
+    }
+
+    private fun additionChest(location: Location) {
+        val chestList = ymlMaterial.acquisitionListValue(gameData, key)
+        val chestLocation = "x:${location.x}y:${location.y}z:${location.z}"
+        chestList?.add(chestLocation)
+        ymlMaterial.setValue(gameData, key, chestList)
+    }
+
+    private fun deletionChest(location: Location) {
+        val chestList = ymlMaterial.acquisitionListValue(gameData, key)
+        val chestLocation = "x:${location.x}y:${location.y}z:${location.z}"
+        chestList?.remove(chestLocation)
+        ymlMaterial.setValue(gameData, key, chestList)
+    }
+}
