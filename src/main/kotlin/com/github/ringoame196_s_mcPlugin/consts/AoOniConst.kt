8764a6c79672ff5972be27974c6b58af4ddbc11e
りんご飴196 <kazumac41@gmail.com>
2024-08-23 17:22:19 +0900

    [追加] 設定可能箇所

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/consts/AoOniConst.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/consts/AoOniConst.kt
@@ -1,6 +1,12 @@
 package com.github.ringoame196_s_mcPlugin.consts
 
 object AoOniConst {
+    const val AO_ONI_CHEST_KEY = "ao_oni_chests" // チェストのKey
+    const val AO_ONI_WORLD_KEY = "ao_oni_world" // 青鬼ワールドのKey
+
     const val AO_ONI_TEAM_NAME = "aooni" // 青鬼チーム名
     const val HIROSHI_TEAM_NAME = "hiroshi" // ひろしチーム名
+
+    const val SETTING_HIROSHI_START_LOCATION = "hiroshi_start_location" // ひろしのスタート位置
+    const val SETTING_AO_ONI_START_LOCATION = "ao_oni_start_location" // 青鬼のスタート位置
 }
