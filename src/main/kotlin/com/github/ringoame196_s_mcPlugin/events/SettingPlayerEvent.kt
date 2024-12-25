707cb14fe1ccde0a6da2fc800131d08ee7118c2c
りんご飴196 <kazumac41@gmail.com>
2024-12-25 22:23:37 +0900

    ⬆️  :名称変更

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/SettingPlayerEvent.kt
@@ -0,0 +1,9 @@
+package com.github.ringoame196_s_mcPlugin.events
+
+import com.github.ringoame196_s_mcPlugin.managers.YmlManager
+import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
+
+class SettingPlayerEvent(plugin: Plugin) : Listener {
+    private val ymlMaterial = YmlManager()
+}
